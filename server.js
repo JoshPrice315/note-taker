3765b8903f84e5b72b849702cecdb9d4499e6d58
JoshPrice315 <61852564+JoshPrice315@users.noreply.github.com>
2020-05-24 23:24:11 -0700

    Heroku

---
+++ server.js
@@ -0,0 +1,98 @@
+
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+
+// port 3001
+var PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes", function (req, res) {
+    let theJSON = null;
+    fs.readFile("./db/db.json", function (err, data) {
+        console.log(data);
+        if (err) return console.error(err);
+        theJSON = JSON.parse(data);
+        for (let i = 0; i < theJSON.length; ++i) {
+            theJSON[i].id = i + 1;
+        }
+        res.json(theJSON);
+    });
+});
+
+app.post("/api/notes", function (req, res) {
+    var newNote = req.body;
+    console.log(newNote);
+    // let theJSON = null;
+    fs.readFile("./db/db.json", function (err, data) {
+        if (err) return console.error(err);
+        let theJSON = JSON.parse(data);
+        console.log(theJSON);
+        // newNote.id = theJSON.length + 1;
+        theJSON.push(newNote);
+        write(theJSON);
+    });
+
+    function write(theJSON) {
+        fs.writeFile("./db/db.json", JSON.stringify(theJSON), function (err, data) {
+            if (err) return console.log(err);
+            console.log("saved");
+        });
+        let responder = {
+            noteTitle: req.body.title,
+            noteContent: req.body.text,
+            ID: theJSON.length
+        };
+
+        res.json(responder);
+    }
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+    let id = parseInt(req.params.id);
+    console.log(id);
+
+    if (isNaN(id)) {
+        res.sendStatus(400);
+        return;
+    }
+
+    fs.readFile("./db/db.json", function (err, data) {
+        if (err) return console.error(err);
+        let theJSON = JSON.parse(data);
+        console.log(theJSON);
+
+        if (id > theJSON.length || id == 0) {
+            res.sendStatus(403);
+            return;
+        }
+
+        theJSON.splice(id - 1, 1);
+        console.log(theJSON);
+        write(theJSON);
+    });
+
+    function write(theJSON) {
+        fs.writeFile("./db/db.json", JSON.stringify(theJSON), function (err, data) {
+            if (err) return console.log(err);
+            console.log("saved");
+        });
+        res.sendStatus(200);
+    }
+});
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
